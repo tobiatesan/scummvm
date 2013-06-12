tobiatesan/scummvm

commit a25b462889541ce5453955ca748985e4ab5079f8

WINTERMUTE: Prevent warning about duplicate files when autodetecting.
Removed a call to BaseFileManager::registerPackages() in
WintermuteEngine::getGameInfo(), as the constructor for BaseFileManager
already calls registerPackages() once; another call should thus be
superfluous.

engines/wintermute/wintermute.cpp
@@ -292,7 +292,6 @@ bool WintermuteEngine::getGameInfo(const Common::FSList &fslist, Common::String 
 	Common::SeekableReadStream *stream = nullptr;
 	// Quick-fix, instead of possibly breaking the persistence-system, let's just roll with it
 	BaseFileManager *fileMan = new BaseFileManager(Common::UNK_LANG, true);
-	fileMan->registerPackages(fslist);
 	stream = fileMan->openFile("startup.settings", false, false);
 
 	// The process is as follows: Check the "GAME=" tag in startup.settings, to decide where the

commit db0f0c80d829914f157aa43c8f1ac160fad0dc5c

WINTERMUTE: Fix autodetection when adding a game.
Fix a bug introduced in a25b462889541ce5453955ca748985e4ab5079f8 that broke autodetection when adding a game.

1. Reinstate fileMan->registerPackages(fslist); in Wintermute::getGameInfo, as it *is* needed when adding a game.
2. Tackle the issue the previous commit was meant to fix one layer above, in advancedDetector.cpp.
An empty fslist is passed to fallbackDetect when starting a game that's already been added so that we don't pass the same files twice.
The piece of code in question is not called when adding a game.

engines/advancedDetector.cpp
@@ -266,7 +266,10 @@ Common::Error AdvancedMetaEngine::createInstance(OSystem *syst, Engine **engine)
 
 	if (agdDesc == 0) {
 		// Use fallback detector if there were no matches by other means
-		agdDesc = fallbackDetect(allFiles, files);
+		Common::FSList empty_fslist; 
+		// We don't need to pass the same files twice, once in the FileMap, once in 
+		// an FSList, otherwise we get warnings.
+		agdDesc = fallbackDetect(allFiles, empty_fslist);
 		if (agdDesc != 0) {
 			// Seems we found a fallback match. But first perform a basic
 			// sanity check: the gameid must match.

engines/wintermute/wintermute.cpp
@@ -292,6 +292,7 @@ bool WintermuteEngine::getGameInfo(const Common::FSList &fslist, Common::String 
 	Common::SeekableReadStream *stream = nullptr;
 	// Quick-fix, instead of possibly breaking the persistence-system, let's just roll with it
 	BaseFileManager *fileMan = new BaseFileManager(Common::UNK_LANG, true);
+	fileMan->registerPackages(fslist);
 	stream = fileMan->openFile("startup.settings", false, false);
 
 	// The process is as follows: Check the "GAME=" tag in startup.settings, to decide where the